1e77afbffd44e3e29e96cb5ed7795fb74813a963
kenkwon <jrrnjs@gmail.com>
2019-02-13 15:25:21 +0900

    photo_detail

--- lib/ui/home/tab/photo_page.dart
+++ lib/ui/home/tab/photo_page.dart
@@ -24,6 +24,8 @@ class PhotoTabPage extends StatefulWidget {
     'sky'
   ];
 
+  String _currentKeyword;
+
   @override
   State createState() {
     return _PhotoTabPageState();
@@ -32,8 +34,8 @@ class PhotoTabPage extends StatefulWidget {
 
 class _PhotoTabPageState extends State<PhotoTabPage> {
 
-  void _getImages(String keyword) async {
-    var response = await FlickrApi.getImages(keyword);
+  void _getImages() async {
+    var response = await FlickrApi.getImages(widget._currentKeyword);
     setState(() {
       final cutBody = response.body.substring(14, response.body.length - 1);
       var responseBody = (json.decode(cutBody) as Map<String, dynamic>);
@@ -46,31 +48,62 @@ class _PhotoTabPageState extends State<PhotoTabPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.extent(
-        maxCrossAxisExtent: 150.0,
+      body: GridView.count(
+        crossAxisCount: 3,
         mainAxisSpacing: 5.0,
         crossAxisSpacing: 5.0,
         children: _buildGridTiles(),
+        controller: ScrollController(
+
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.refresh),
         onPressed: () {
           final index = Random.secure().nextInt(widget._randomKeyword.length-1);
-          _getImages(widget._randomKeyword[index]);
+          widget._currentKeyword = widget._randomKeyword[index];
+          _getImages();
         },
       ),
     );
   }
 
   List<Widget> _buildGridTiles() {
-    List<Container> containers =
-        List<Container>.generate(widget._items.length, (index) {
+    List<GridTile> tiles = List<GridTile>.generate(widget._items.length,
+    (index) {
       final item = widget._items[index];
-      return Container(
-        child: new Image.network(item.thumbnailUrl, fit: BoxFit.cover),
+      final Widget image = Image.network(item.thumbnailUrl, fit: BoxFit.cover);
+
+      return GridTile(
+        child: GestureDetector(
+          child: image,
+          onTap: () { _showPhotoDetail(item.photoUrl); },
+        )
       );
     });
-    return containers;
+    return tiles;
+  }
+
+  void _showPhotoDetail(String photoUrl) {
+    final route = MaterialPageRoute(
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget._currentKeyword),
+            ),
+            body: Container(
+              color: Colors.black,
+              child: Center(
+                child: Image.network(
+                  photoUrl,
+                  fit: BoxFit.fitWidth
+                ),
+              ),
+            ),
+          );
+        }
+    );
+    Navigator.of(context).push(route);
   }
 }
 
